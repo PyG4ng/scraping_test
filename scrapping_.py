94ea05e24f4b397100fa004a1aa7966579484c7b
Daniel <danielslackpy@gmail.com>
2021-10-29 14:55:41 +0300

    Getting all the lyrics, next step optimizing speed

--- scrapping_.py
+++ scrapping_.py
@@ -10,7 +10,7 @@ from pprint import pprint
 def get_all_songs_url():
     next_page = 1
     urls = []
-    while next_page <= 10:
+    while next_page:
         r = requests.get(f'https://genius.com/api/artists/835/songs/search?page={next_page}&q=michael+jackson&sort=popularityy')
         contenu = r.json().get("response")
         next_page = contenu.get('next_page', "Not found")
@@ -19,18 +19,32 @@ def get_all_songs_url():
     # pprint(urls)
     return urls
 
+# Get all the songs links
+# with open("all_songs_links.json", "w") as f:
+#         json.dump(get_all_songs_url(), f, indent=4)
+
+# get_all_songs_url()
+
 def get_all_lyrics(links):
     lyrics_song = []
     for link in links:
+        x = 0
         results = None
         while results == None:
             print(f"trying link number {links.index(link)}")
             song_page = requests.get(link)
             soup = BeautifulSoup(song_page.content, "html.parser")
             results = soup.find(id="lyrics-root")
-        lyrics_song.extend([el for el in results.stripped_strings if "[" not in el and "]" not in el and el not in ["Share URL", "Copy","Embed"]][:-1])
+            x +=1
+            if x == 10:
+                break
+        if x != 10:
+            lyrics_song.extend([el for el in results.stripped_strings if "[" not in el and "]" not in el and el not in ["Share URL", "Copy","Embed"]][:-1])
         # pprint(lyrics_song)
-    with open("songs.json", "w") as f:
-        json.dump(lyrics_song, f, indent=4)
+    return lyrics_song
 
-get_all_lyrics(get_all_songs_url())
+# Get all the lyrics in all the song links   
+with open("songs.json", "w") as f:
+        json.dump(get_all_lyrics(get_all_songs_url()), f, indent=4)
+
+# get_all_lyrics(get_all_songs_url())
