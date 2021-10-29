06e10f6934d9792aef0aed18741f3b985a50a29e
Daniel <danielslackpy@gmail.com>
2021-10-29 14:13:07 +0300

    Testing

--- scrapping_.py
+++ scrapping_.py
@@ -10,27 +10,31 @@ from pprint import pprint
 def get_all_songs_url():
     next_page = 1
     urls = []
-    while next_page <= 10:
+    while next_page:
         r = requests.get(f'https://genius.com/api/artists/835/songs/search?page={next_page}&q=michael+jackson&sort=popularityy')
         contenu = r.json().get("response")
         next_page = contenu.get('next_page', "Not found")
         print(next_page)
         urls.extend([el['url'] for el in contenu.get('songs')])
     # pprint(urls)
+    with open("links.json", "w") as f:
+        json.dump(urls, f, indent=4)
     return urls
 
-def get_all_lyrics(links):
-    lyrics_song = []
-    for link in links:
-        results = None
-        while results == None:
-            print(f"trying link number {links.index(link)}")
-            song_page = requests.get(link)
-            soup = BeautifulSoup(song_page.content, "html.parser")
-            results = soup.find(id="lyrics-root")
-        lyrics_song.extend([el for el in results.stripped_strings if "[" not in el and "]" not in el and el not in ["Share URL", "Copy","Embed"]][:-1])
-        # pprint(lyrics_song)
-    with open("songs.json", "w") as f:
-        json.dump(lyrics_song, f, indent=4)
+get_all_songs_url()
 
-get_all_lyrics(get_all_songs_url())
+# def get_all_lyrics(links):
+#     lyrics_song = []
+#     for link in links:
+#         results = None
+#         while results == None:
+#             print(f"trying link number {links.index(link)}")
+#             song_page = requests.get(link)
+#             soup = BeautifulSoup(song_page.content, "html.parser")
+#             results = soup.find(id="lyrics-root")
+#         lyrics_song.extend([el for el in results.stripped_strings if "[" not in el and "]" not in el and el not in ["Share URL", "Copy","Embed"]][:-1])
+#         pprint(lyrics_song)
+#     with open("songs.json", "w") as f:
+#         json.dump(lyrics_song, f, indent=4)
+
+# get_all_lyrics(get_all_songs_url())
